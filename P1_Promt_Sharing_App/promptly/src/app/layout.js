48544c4e44e82e2bbbaa45e2b73142c7ad9a192a
Mubeen006 <cmubeen599@gmai.com>
2025-02-22 20:59:36 +0500

    creating needed compnents, and home hero section,

--- P1_Promt_Sharing_App/promptly/src/app/layout.js
+++ P1_Promt_Sharing_App/promptly/src/app/layout.js
@@ -19,10 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
       </body>
     </html>
   );
--- P1_Promt_Sharing_App/promptly/src/app/page.js
+++ P1_Promt_Sharing_App/promptly/src/app/page.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Promptly
-    </div>
+    <section className="w-full items-center justify-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+      </h1>
+      <p className='desc text-center'>Promptopia is an open-source AI promting
+        tool for modern world to discover, create 
+        and share creative prompts
+      </p>
+      {/* Feed*/}
+    </section>
   )
 }
 
